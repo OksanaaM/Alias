3cddf66f4d179c7c1bb7dc2e364526a6cd00781a
OksanaaM <kseniamikolenko7@gmail.com>
2016-05-10 18:13:36 +0300

    Add files via upload

---
+++ GameOfFourTeams.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Оксана on 10.05.2016.
+ */
+public class GameOfFourTeams extends Team {
+    public void startGame(){
+
+    }
+}
---
+++ Team.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Оксана on 10.05.2016.
+ */
+public class Team {
+    int point;
+    String name;
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+}
